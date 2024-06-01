e16142ac39dfec246f0ab7758dc1680a32e8c1bd
painkhn <1fotice@gmail.com>
2024-06-01 16:50:40 +0800

    главная страница | страница публикации | логин | регистрация

---
+++ verstka/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "index.html",
+    "./pages/publication.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
