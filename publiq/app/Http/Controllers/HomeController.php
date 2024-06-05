19ba479e979f25dacd5ff982670587140116a07f
10010010 <lll100100@yandex.ru>
2024-06-05 17:00:49 +0800

    fix

--- publiq/app/Http/Controllers/HomeController.php
+++ publiq/app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     }
     public function catalog()
     {
-        $story = Story::get();
+        $story = Story::where('status', 'accepted')->get();
         return view('catalog', ['populars' => $story]);    
     }
 }
