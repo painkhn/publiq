5017354c31d9459e5734cd088836a41573f203ec
painkhn <1fotice@gmail.com>
2024-06-11 17:00:01 +0800

    фикс

--- publiq/resources/views/index.blade.php
+++ publiq/resources/views/index.blade.php
@@ -15,7 +15,7 @@
                         <li>
                             <div class="max-w-lg">
                                 <div
-                                    class="public-block w-full min-h-64 border-4 border-primary rounded-md text-center px-5 py-8 color-grey mb-2">
+                                    class="public-block w-full h-64 border-4 border-primary rounded-md text-center px-5 py-8 color-grey mb-2">
                                     <div class="public-title text-xl mb-5">
                                         <h3>{{ $popular->name }}</h3>
                                     </div>
@@ -43,7 +43,7 @@
                         <li>
                             <div class="max-w-lg">
                                 <div
-                                    class="public-block w-full min-h-64 border-4 border-primary rounded-md text-center px-5 py-8 color-grey mb-2">
+                                    class="public-block w-full h-64 border-4 border-primary rounded-md text-center px-5 py-8 color-grey mb-2">
                                     <div class="public-title text-xl mb-5">
                                         <h3>{{ $story->name }}</h3>
                                     </div>
